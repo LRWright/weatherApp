5371e6f4f7e4ae438fc1940b488258b60a6619fb
Le-Ann Wright <leannwright21@gmail.com>
2021-03-03 17:29:42 +0000

    Local files pushed to git

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const getWeather = require('./lib/weather');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', hbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+    let data = getWeather();
+    res.render('index', {data});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000");
+});
